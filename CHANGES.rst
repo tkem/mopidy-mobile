b1f60d81e9ba710ee428cccc6badb671a1777a6b
Thomas Kemmer <tkemmer@computer.org>
2015-01-26 18:39:04 +0100

    Prepare v0.1.0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-0.1.0 UNRELEASED
+0.1.0 2015-01-26
 ----------------
 
 - Initial release.
--- mopidy_mobile/__init__.py
+++ mopidy_mobile/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from mopidy import config, ext
 
-__version__ = '0.0.6'
+__version__ = '0.1.0'
 
 
 class Extension(ext.Extension):
