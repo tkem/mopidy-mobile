eefada1b55a165bcdd4ae1c32170182da90754d8
Thomas Kemmer <tkemmer@computer.org>
2015-04-25 07:50:14 +0200

    Fix #43: Update Android APK build for latest ionic/cordova versions.

--- mopidy_mobile/__init__.py
+++ mopidy_mobile/__init__.py
@@ -4,7 +4,7 @@ import os
 
 from mopidy import config, ext
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 
 
 class Extension(ext.Extension):
--- scripts/release-apk.sh
+++ scripts/release-apk.sh
@@ -4,8 +4,8 @@
 #
 JARSIGNER="jarsigner"
 ZIPALIGN="$ANDROID_HOME/build-tools/21.1.2/zipalign"
-INFILE="${3:-platforms/android/ant-build/CordovaApp-release-unsigned.apk}"
-OUTFILE="${4:-platforms/android/ant-build/CordovaApp-release.apk}"
+INFILE="${3:-platforms/android/build/outputs/apk/android-release-unsigned.apk}"
+OUTFILE="${4:-platforms/android/build/outputs/apk/android-release.apk}"
 
 "$JARSIGNER" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$1" "$INFILE" "$2"
 "$ZIPALIGN" -v -f 4 "$INFILE" "$OUTFILE"
